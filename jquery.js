21d778d81d186ef71939691ef1f5b1f57db08d98
Đàm Hữu Thành <nklove070902@gmail.com>
2022-03-29 11:00:56 +0700

    jquery

---
+++ jquery.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+	const checkLength = (inputElement, spanElement, start, end) => {
+		inputElement.blur(function () {
+			const value = inputElement.val().length
+			value >= start && value <= end
+				? (spanElement[0].textContent = undefined)
+				: (spanElement[0].textContent = `Nhận độ dài từ ${start} đến ${end}`)
+		})
+	}
+
+	checkLength($('#txUserID'), $('#statusOfUserID'), 5, 12)
+	checkLength($('#txPassword'), $('#statusOfPassword'), 7, 12)
+
+	const alphabate = /^[a-zA-Z]+$/
+	const numeric = /^[0-9]+$/
+	const email =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+	const checkOnly = (inputElement, regex, statusElm, contentError) => {
+		inputElement.blur(function () {
+			const value = regex.test(inputElement.val())
+			value
+				? (statusElm[0].textContent = undefined)
+				: (statusElm[0].textContent = contentError)
+		})
+	}
+
+	checkOnly(
+		$('#txName'),
+		alphabate,
+		$('#statusOfName'),
+		'Chỉ được chữ và ghi liền không dấu'
+	)
+	checkOnly(
+		$('#txZipCode'),
+		numeric,
+		$('#statusOfZipCode'),
+		'Chỉ nhận giá trị số'
+	)
+	checkOnly(
+		$('#txEmail'),
+		email,
+		$('#statusOfEmail'),
+		'Vui lòng nhập đúng email'
+	)
+})
+ 
